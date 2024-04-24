ad2dccc3bc964dde04a2c917eac06238cf8101bd
kiianskii <ankiyanskiy@gmail.com>
2024-04-24 22:01:13 +0300

    fixed

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,12 @@
-let some:unknown;
+let some: unknown;
+
 some = 'Text';
-// let str: string;
-let str = some as string;
+
+let str: string;
+
+if (typeof some === 'string') {
+    str = some;
+}
+
 
 export {};
