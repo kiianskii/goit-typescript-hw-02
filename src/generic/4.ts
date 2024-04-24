21ce69088b3702ba700ed39271181b652b0c3aa9
kiianskii <ankiyanskiy@gmail.com>
2024-04-24 18:34:04 +0300

    done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,18 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface ComponentObj {
+    title: string;
+    [props: string]: string | number | boolean;
+}
+
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <ComponentObj> {
   pageInfo () {
     console.log(this.props.title);
   }
